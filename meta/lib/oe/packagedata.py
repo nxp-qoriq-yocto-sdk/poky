1d91a59ae8d1d7b6ba1a11a028884a95bcc71343
Matthew McClintock <msm@freescale.com>
2012-03-08 14:13:40 -0600

    packagedata.py: Fix get_subpkgedata_fn for multilib
    Signed-off-by: Matthew McClintock <msm@freescale.com>

--- meta/lib/oe/packagedata.py
+++ meta/lib/oe/packagedata.py
@@ -27,6 +27,14 @@ def read_pkgdatafile(fn):
 
 def get_subpkgedata_fn(pkg, d):
     archs = bb.data.expand("${PACKAGE_ARCHS}", d).split(" ")
+    mlarchs = bb.data.getVar("MULTILIB_PACKAGE_ARCHS", d) or None
+
+    if mlarchs:
+        for mlarch in mlarchs.split(" "):
+            if "_" in mlarch:
+                prefix, split, new_arch = mlarch.partition("_")
+                archs.append(new_arch)
+
     archs.reverse()
     pkgdata = bb.data.expand('${TMPDIR}/pkgdata/', d)
     targetdir = bb.data.expand('${TARGET_VENDOR}-${TARGET_OS}/runtime/', d)
